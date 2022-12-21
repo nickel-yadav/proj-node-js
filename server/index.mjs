5966ce39f9b03d964a83949027ff2555944aa62f
Nikhil <nickelyadav57@gmail.com>
2022-12-21 07:37:59 +0530

    feat: Adds dynamic path to rest api

---
+++ server/index.mjs
@@ -0,0 +1,35 @@
+import express from 'express'
+import bp from 'body-parser'
+import morgan from 'morgan'
+
+const app = express();
+
+app.use(bp.urlencoded({extended: true}))
+app.use(bp.json())
+app.use(morgan('dev'))
+
+const db = []
+
+app.post('/todo', (req, res) => {
+    const newTodo = {
+        id: Date.now(),
+        text: req.body.text
+    }
+
+    db.push(newTodo);
+
+    res.json(newTodo)
+
+})
+
+app.get('/todo/:id', (req, res) => {
+    const todo = todos.find(t => {
+        return t.id === +req.params.id
+    })
+
+    res.json({data: todo});
+})
+
+app.listen(8000, () => {
+    console.log('server on 8000')
+})
